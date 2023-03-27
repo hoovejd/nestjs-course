9a2d591ebd8591a4a0ca31e293ca2cd2f791d9f9
Jarrod Hoover <hoov85@gmail.com>
2023-03-27 14:39:54 -0400

    refactored serialize interceptor

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -1,18 +1,24 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    // Run something before the request is handled by the request handler
-    console.log('#1 Im running before the handler', context);
+    // Run something here before the request is handled by the request handler in the controller
 
     return next.handle().pipe(
       map((data: any) => {
-        // Run something before the response is sent out
-        console.log('#3 Im running before response is sent out', data);
+        // Run something here before the response is sent out, after the controller request handler runs
+
+        // excludeExtraneousValues will only return DTO properties decorated with @Expose!
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
